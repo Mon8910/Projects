8a089770c8966e64c3069944c551a6b3485d463a
Ayman Mohamed <107960871+Mon8910@users.noreply.github.com>
2022-11-16 23:52:48 +0200

    Add files via upload

---
+++ quiz ayman mohamed.py
@@ -0,0 +1,853 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Question 1: 
+# 
+# What is AI? 
+
+# Artificial Intelligence is branch of computer science ,development of intelligence machines ,machines do things like human in industry , medical & engineering
+
+# ### Question 2: 
+# Why AI is used ? 
+
+# because , in this days ,data is very big that it is incomprehensible for human and machine so use machine learning ,deep learning to solve this problem to analyziz measure of data 
+
+# ### Question 3:
+# When to use AI ? 
+
+# when you have a big data to solve the problem and also use in every thing in the life to slove anything like traffic problems
+
+# ### Question 4:
+# 
+# Define the AI pipeline
+
+# In[1]:
+
+
+1-verify
+2-data Collection
+3-data Preprocessing
+4-feature Engineerin
+5-model Selection
+6-fitting
+7-training
+8-testing
+9-save model
+10-Documentation
+11-integration
+12-monitoring
+13-maintance
+
+
+# ### Question 5:
+# 
+# Define the general sequence of Software Engineering...
+
+# software engineering is software devlopment cycle like analyziz ,design , dvolpment,integration, implemention and maintance
+
+# ### Question 6: 
+# 
+# Why DivOps is one of the most used cycles in software engineering?
+
+# beacuse it make that increasing in organization that  ability to deliver applications and services at high velocity
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Question 7
+# What is software Engineering ?
+
+# In[ ]:
+
+
+software engineering :is the fileld that do to create many types to solve any problems like mobile aplication 
+    web aplication , ai and embedded system
+
+
+# ### Question 8
+# 
+# Define MLOPS 
+
+# mlops : machine learening operations is the function of machine learening and foucs of streaming of process
+
+# ### Question 9
+# Define DataOps
+
+#  dataops is a collection of technical practices and workflows 
+
+# ### Question 10
+# 
+# AI is ....% Data Operations , and ..... Model Creation 
+
+# In[ ]:
+
+
+representation of how an organizations components are organized and function together to execute on strategy
+
+
+# 
+
+# ### Question 11
+# 
+# Give 5 Examples of AI driven Companies in Egypt
+# 
+
+# In[ ]:
+
+
+1-vodafone
+2-ibm
+3-etislat
+4-wuzzuf
+5-agolo
+
+
+# 
+
+# ### Question 12
+# What are the main fields of AI in Egypt?
+# 
+
+# In[ ]:
+
+
+machine learning ,deep learening, neural nerworks ,robotics
+
+
+# 
+
+# ### Question 13
+# Define Data Science
+
+# In[ ]:
+
+
+data scinence is the study of data to extract meaningful insights to bussines 
+and also it contains many fields like mathmatics ,statistics and artificial intelligence
+and use to analyziz large data
+
+
+# 
+
+# ### Question 14 
+# What is the difference between Data Engineer, Data Analyst and Data Scientist 
+
+# In[ ]:
+
+
+data analyst : collect data ,clean and analysis data and aslo cheack the valied of data 
+    data engineer :collect data ,mange and convert it to rows to use in data science
+    the role of data science is gather data , statical analyziz ,machine learening , deep learening and programming    
+
+
+# 
+
+# ### Question 15
+# What are the most important skills an AI Engineer must have? 
+
+# In[ ]:
+
+
+1-programming like python or c++
+2-mathmatics like linear algebra
+3-machine learning
+4- deep learing
+5-neural networks
+
+
+# 
+
+# ### Question 16: ( Bonus ) 
+# Who is my favorite Character in My favoirte Show 
+
+# In[ ]:
+
+
+Heisenberg
+
+
+# 
+
+# ### Question 17: ( Bonus )
+# What is my favorite TV Show
+
+# In[ ]:
+
+
+the good doctor or  breaking bad 
+
+
+# 
+
+# ### Question 18: 
+# What are the possible methods to collect our data? 
+
+# In[ ]:
+
+
+1-interviews
+2-surveys 
+3-questionnaires
+4-records
+
+
+# 
+
+# ### Question 19: 
+# What is Feature Engineering? 
+
+# In[ ]:
+
+
+Feature engineering is the process of selecting, manipulating, and transforming raw data
+   that can be used in supervised 
+
+
+# 
+
+# ### Question 20: 
+# What are the main fields of mathematics used in AI? 
+
+# In[ ]:
+
+
+1-linear algebra
+2-calculus
+3-statics 
+4-Probability
+
+
+# 
+
+# ### Question 21:
+# What is Data Sampling ?
+
+# In[ ]:
+
+
+data sampling is analyziz of unsubset data that incover meaningfulin large of data set
+
+
+# 
+
+# ### Question 22: 
+# What is Sample Size ?
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# ### Question 23: 
+# How to avoid bad Sampling
+
+# In[ ]:
+
+
+increase sample size then define a target population and a sampling frame then avoid convenience sampling
+
+
+# 
+
+# ### Question 24:
+# What are the acceptable range of sampling ?
+
+# In[ ]:
+
+
+10% or
+10% tp 30%
+
+
+# 
+
+# ### Question 25:
+# What AI is trying to find when working with data? 
+# 
+
+# In[ ]:
+
+
+ai can ask questions, discover and test hypotheses, and make decisions based on advanced analytics
+operating on data sets
+
+
+# 
+
+# ### Question 26:
+# What are the problem of generalizing a result obtained by a sample into the whole data ? 
+
+# In[ ]:
+
+
+the problem is educational research, and throughout the social sciences
+
+
+# 
+
+# ### Question 27:
+# Why do we do Sampling?
+# 
+
+# In[ ]:
+
+
+because sampling used to make interface about populations 
+and also they are practical, cost effective, convenient, and manageable
+
+
+# 
+
+# ### Question 28: 
+# Define Statistical Test
+
+# In[ ]:
+
+
+statistical test: provides a mechanism for making quantitative decisions about a process or processess
+
+
+# 
+
+# ### Question 29: 
+# Suggest methods to work with small data 
+
+# In[ ]:
+
+
+Few Shot Learning
+knowledge graphs
+transfer learning
+self supervised learning
+synthetic data
+
+
+# 
+
+# ### Question 30: 
+# What are the types of data?
+
+# In[ ]:
+
+
+quantitative
+qulative
+
+
+# 
+
+# ### Question 31: 
+# What is a programming language?
+
+# In[ ]:
+
+
+programming language is a set of instructions following the rules of the chosen language.
+
+
+# 
+
+# ### Question 32: 
+# Give 2 examples of low level programming languages:
+
+# In[ ]:
+
+
+assembly language and machine language , c
+
+
+# 
+
+# ### Question 33: ( Bonus ) 
+# What is the difference between java and JavaScript ?
+
+# In[ ]:
+
+
+java : is oop programming language
+java script:is oop scripting language    
+
+
+# 
+
+# ### Question 34: 
+# Give a complete definition for python as a programming language
+
+# In[ ]:
+
+
+python is programming language , oop programming language ,high levl language and interprete
+
+
+# 
+
+# ### Question 35:
+# Why do we use python for AI ?
+
+# In[ ]:
+
+
+first : contain many libraries of ai to solve any problems 
+    second : machine learning need solving complex algorithms
+
+
+# 
+
+# ### Question 36
+# What are the most used programming languages for AI in business? and Why ?  ( State only 3 )
+python beacuse it is very easy to use 
+c++
+R
+haskal
+# 
+
+# ### Question 37: 
+# Can we use Simple Machine learning with image data ? and why / why not ? 
+
+# In[ ]:
+
+
+yes because machine learning algorithms allow to do image processing at scale, and with great detail
+
+
+# 
+
+# ### Question 38: 
+# What are the basic data types of python ? 
+
+# In[ ]:
+
+
+int 
+float
+string 
+boolean 
+
+
+# ### Question 39: 
+# What are the basic containers in python ( Data Structure Elements )
+
+# list , tupples , dictionary
+
+# ### Question 40: 
+# What are the most important packages for python to work with AI ? ( give at least 6 )
+
+# In[ ]:
+
+
+pandas 
+numpy
+requests 
+Selenium
+Beatiful soup
+tensorFlow
+
+
+# 
+
+# ### Question 41: 
+# What is knowledge domain in data science? 
+
+# In[ ]:
+
+
+knowledge domain is used to refer to the general background knowledge of the field or enviroments
+
+
+# 
+
+# ### Question 42: 
+# Create a list of theses elements : 1,3,4,16,17,20, "Ahmed", "Mazen", True
+
+# In[2]:
+
+
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+l
+
+
+# 
+
+# ### Question 43: 
+# add 7 to the list 
+
+# In[3]:
+
+
+l.append(7)
+l
+
+
+# 
+
+# ### Question 44: 
+# remove 3 from the list 
+
+# In[4]:
+
+
+l.remove(3)
+l
+
+
+# 
+
+# ### Question 45: 
+# Search for "Ahmed" in the list 
+
+# In[29]:
+
+
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+element="ahmed"
+for i in range(len(l)):
+    if l[i]==element:
+        print("find")
+    else :
+        print("error")
+print("   ")        
+#### another solution
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+element="ahmed"
+f=0
+for i in range(len(l)):
+    if l[i]==element:
+        print("findd")
+        f=1
+        break 
+if(f==0):
+    print("not find")
+
+
+# ### Question 46: 
+# Count how many numbers 4 has occured
+
+# In[7]:
+
+
+l.count(4)
+print(l.count(4))
+
+
+# 
+
+# ### Question 47:
+# print only the even numbers in the list
+
+# In[11]:
+
+
+for i in l:
+    if i%2==0 :
+        print(i)
+    
+
+
+# 
+
+# ### Question 48: 
+# Sum only the numeric elements in the list
+
+# In[40]:
+
+
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+al=l[0:4]
+al
+s=0
+for i in range(0,len(al)):
+    s=s+al[i]
+print(s)
+
+
+# 
+
+# ### Question 49: 
+# convert the list into a set 
+
+# In[48]:
+
+
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+s=set(l)
+s
+
+
+# 
+
+# ### Question 50:
+# change the first element to be 25 then print the new list
+
+# In[52]:
+
+
+l=[1,3,4,16,17,20,"ahmed","mazen",True]
+l[0]=25
+l
+
+
+# 
+
+# ### Question 51: 
+# Assign only the first 5 values of the list into a new list called AI 
+
+# In[53]:
+
+
+Al=l[0:5]
+Al
+
+
+# 
+
+# ### Question 52: 
+# Print the type of each element ( Hint: Use for loop )
+
+# In[57]:
+
+
+l
+for i in l :
+    print(type(i))
+
+
+# 
+
+# ### Question 53: 
+# Suppose you have a file called new data.csv , write the python code to read this data 
+
+# In[ ]:
+
+
+import pandas as pd 
+import numpy as np 
+df=pd.read("new data.csv")
+df
+
+
+# 
+
+# ### Question 54: 
+# print its columns 
+
+# In[ ]:
+
+
+df.columns
+
+
+# 
+
+# ### Question 55: 
+# What are the packages used for web scrapping? 
+
+# In[ ]:
+
+
+Requests, BeautifulSoup,Selenium
+
+
+# 
+
+# ### Question 56: 
+# What are the types of learning ?
+
+# In[ ]:
+
+
+supervised, unsupervised, and reinforcement learning
+
+
+# 
+
+# ### Question 57: 
+# What are the types of problems AI Can solve ? 
+
+# In[ ]:
+
+
+Self-driving Vehicle
+Healthcare
+Cybersecurity
+Voice Assistants
+or 
+medical 
+software engineering
+industry
+
+
+# 
+
+# ### Question 58:
+# What is Data Augmentation?
+
+# In[ ]:
+
+
+Data Augmentation used in deeplearning then
+is a process of artificially increasing the amount of data by generating new data points from existing data
+
+
+# 
+
+# ### Question 59  (Bonus) : 
+# What are the types of medical image files?  
+
+# In[ ]:
+
+
+pdf
+jpg
+psd
+eps
+
+
+# 
+
+# ### Question 60: ( Bonus ) 
+# What are the 7Vs of Big Data? 
+
+# volume, velocity, variety, variability, veracity, value, and visibility
+
+# ### Question 61:
+# What is Dimensionality reduction
+
+# In[ ]:
+
+
+itis a machine learning or statistical technique of reducing the amount of random variables in a problem
+
+
+# ### Question 62:
+# What are the main features of Time Seires Analysis
+
+# In[ ]:
+
+
+trends.
+seasonal and nonseasonal cycles
+pulses and steps
+outliers
+
+
+# ### Question 62:
+# you're given X,y and  
+# x = [1,3,4,5,6,7,9]
+# y =[1,3,2,2,3,7,9]
+# z = ["A" , "B",  "C" , "D", "E" , "F" , "G"] 
+# 
+# create: 
+# 1. plot chart 
+# 2. bar chart
+# 3. pie chart
+# 
+
+# ### Question 63: 
+# Read image from your directory and display it here 
+
+# In[70]:
+
+
+import matplotlib as plt
+import numpy as np
+z = ["A" , "B",  "C" , "D", "E" , "F" , "G"]
+x = [1,3,4,5,6,7,9]
+y =[1,3,2,2,3,7,9]
+plt.plot(x, z)
+
+plt.show()
+plt.plot(x,y)
+plt.show()
+plt.plot(y,z)
+plt.show()
+plt.bar(x, y)
+plt.show()
+plt.bar(x,z)
+plt.show()
+plt.bar(y,z)
+plt.show()
+plt.pie(x)
+plt.show()
+plt.pie(y)
+plt.show()
+
+
+# In[69]:
+
+
+
+
+
+# ### Question 64:
+# Explain Gradient Descent ( with Details ) 
+
+# In[ ]:
+
+
+Gradient descent is an optimization algorithm which is commonly used to train machine learning models and neural networks 
+
+
+# ### Question 65:
+# Create pandas dataframe as input from users
+# 
+# name, 
+# age,
+# id, 
+# gender,
+# email, 
+# phone number, 
+# city,
+# 
+# then 
+# 
+# a. group by city                       
+# b. which city has the most students              
+# c. get all the mails ending with @gmail.com
+# d. get all vodafone phone numbers             
+# e. get the average of ages for each gender               
+# f. plot the relation between number of males to females           
+# g. plot the relation between age and gender             
+
+# In[ ]:
+
+
+columns=[]
+data=dict()
+num=int(input("enter the number of columns"))
+while(num>0):
+    columns.append(input("enter the column name"))
+    num-=1
+for i in columns :
+    data[i]=[]
+rows=int(input("enter the number of the rows:"))
+while(rows>0):
+    for i in data:
+        value=input(f"enter the value of{i}: ")
+        data[i].append(value)
+        rows-=1
+dataframe=pd.DataFrame(data)
+dataframe
+dd=dataframe.groupby("city")
+dd.max()["name"]
+
+
+# ### Best Wishes 
+# ![images.jpg](attachment:images.jpg)
+
+# In[ ]:
+
+
+
+
